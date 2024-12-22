05b6cae3cf2adb83abcfd170d367d1a8af757f41
Doananh2004 <trandoananhnt@gmail.com>
2024-12-22 10:54:00 +0700

    Khởi tạo dự án

---
+++ QuanLyNhaHang/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module vn.trandoananh.quanlynhahang {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens vn.trandoananh.quanlynhahang to javafx.fxml;
+  exports vn.trandoananh.quanlynhahang;
+}
---
+++ QuanLyNhaHang/src/main/java/vn/trandoananh/quanlynhahang/Controller/QuanLyNhaHangController.java
@@ -0,0 +1,6 @@
+package vn.trandoananh.quanlynhahang.Controller;
+
+public class QuanLyNhaHangController {
+
+}
+
