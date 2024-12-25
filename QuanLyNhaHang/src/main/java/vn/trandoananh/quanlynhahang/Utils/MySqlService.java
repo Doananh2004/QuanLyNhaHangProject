fdd22741db42b9f179f2da23c10137bdfe219092
Doananh2004 <trandoananhnt@gmail.com>
2024-12-25 22:09:00 +0700

    Cập nhật MySqlService

--- QuanLyNhaHang/src/main/java/vn/trandoananh/quanlynhahang/Utils/MySqlService.java
+++ QuanLyNhaHang/src/main/java/vn/trandoananh/quanlynhahang/Utils/MySqlService.java
@@ -2,12 +2,9 @@ package vn.trandoananh.quanlynhahang.Utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class MySqlService {
   public Connection conn;
-
   /**
    * Phương thức khởi tạo để thiết lập kết nối cơ sở dữ liệu.
    */
@@ -21,58 +18,6 @@ public class MySqlService {
     } catch (Exception e) {
       e.printStackTrace();
       conn = null;
-    }
-  }
-
-  /**
-   * Phương thức lấy kết nối hiện tại.
-   *
-   * @return Kết nối cơ sở dữ liệu hoặc null nếu kết nối không khả dụng.
-   */
-  public Connection getConnection() {
-    return conn;
-  }
-
-  /**
-   * Đóng tài nguyên ResultSet.
-   *
-   * @param resultSet ResultSet cần đóng.
-   */
-  public void closeResultSet(ResultSet resultSet) {
-    try {
-      if (resultSet != null && !resultSet.isClosed()) {
-        resultSet.close();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Đóng tài nguyên PreparedStatement.
-   *
-   * @param preparedStatement PreparedStatement cần đóng.
-   */
-  public void closePreparedStatement(PreparedStatement preparedStatement) {
-    try {
-      if (preparedStatement != null && !preparedStatement.isClosed()) {
-        preparedStatement.close();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Đóng kết nối cơ sở dữ liệu.
-   */
-  public void closeConnection() {
-    try {
-      if (conn != null && !conn.isClosed()) {
-        conn.close();
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
     }
   }
 }
