ac15c25f08f461203ebb3d92f58b60d0d2f78290
Doananh2004 <trandoananhnt@gmail.com>
2024-12-25 22:16:18 +0700

    Cập nhật GoiMonService

--- QuanLyNhaHang/src/main/java/vn/trandoananh/quanlynhahang/Utils/GoiMonService.java
+++ QuanLyNhaHang/src/main/java/vn/trandoananh/quanlynhahang/Utils/GoiMonService.java
@@ -2,7 +2,6 @@ package vn.trandoananh.quanlynhahang.Utils;
 
 import vn.trandoananh.quanlynhahang.Models.MonAn;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Vector;
